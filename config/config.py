9678c259ce8fc8da5ba8940b75369c837ebc5842
yahyalabser <bestdev29@gmail.com>
2026-07-21 19:11:05 +0100

    fix : auto-refresh du token via Cognito dans fao collector

---
+++ config/config.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_KEY_NEWS = os.getenv("API_KEY_NEWS")
+API_KEY_alpha = os.getenv("API_KEY_alpha")
+FAO_USERNAME = os.getenv("FAO_USERNAME")
+FAO_PASSWORD = os.getenv("FAO_PASSWORD")
